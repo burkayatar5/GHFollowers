9550c0d6c9774f06cc74f35dc9faf800ff4bc046
Burkay Atar <burkayatar5@gmail.com>
2023-12-30 23:03:53 +0300

    SearchVC added

---
+++ GHFollowers/SearchVC.swift
@@ -0,0 +1,18 @@
+//
+//  SearchVC.swift
+//  GHFollowers
+//
+//  Created by Burkay Atar on 30.12.2023.
+//
+
+//
+
+import UIKit
+
+class SearchVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBlue
+    }
+}
