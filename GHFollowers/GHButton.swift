2fb8ec063a0cfd825606828c2faaad7f05e99a8d
Burkay Atar <burkayatar5@gmail.com>
2023-12-30 23:04:18 +0300

    GHButton added.

---
+++ GHFollowers/GHButton.swift
@@ -0,0 +1,35 @@
+//
+//  GHButton.swift
+//  GHFollowers
+//
+//  Created by Burkay Atar on 30.12.2023.
+//
+
+import UIKit
+
+class GHButton: UIButton {
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    init(backgroundColor: UIColor, title: String) {
+        ///handle this with auto layout
+        super.init(frame: .zero)
+        self.backgroundColor = backgroundColor
+        self.setTitle(title, for: .normal)
+        configure()
+    }
+    
+    private func configure() {
+        translatesAutoresizingMaskIntoConstraints = false
+        layer.cornerRadius = 10
+        titleLabel?.textColor = .white
+        titleLabel?.font = UIFont.preferredFont(forTextStyle: .headline)
+    }
+}
