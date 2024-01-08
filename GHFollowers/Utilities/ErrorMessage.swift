fb51b08e8abeaef37bea82bc817b314e4ff564e9
Burkay Atar <burkayatar5@gmail.com>
2024-01-08 13:11:12 +0300

    error message enum added to handle networking errors.

---
+++ GHFollowers/Utilities/ErrorMessage.swift
@@ -0,0 +1,16 @@
+//
+//  ErrorMessage.swift
+//  GHFollowers
+//
+//  Created by Burkay Atar on 4.01.2024.
+//
+
+import Foundation
+
+
+enum GHError: String, Error {
+    case invalidUsername = "This username created an invalid request. Please try again."
+    case unableToComplete = "Unable to complete your request. Please check your internet connection."
+    case invalidResponse = "Invalid response from the server. Please try again."
+    case invalidData = "The data recieved from the server is invalid. Please try again."
+}
