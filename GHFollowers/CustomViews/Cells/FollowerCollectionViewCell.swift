1719c4b34849e86684595aedbd57c6d0353e9f09
Burkay Atar <burkayatar5@gmail.com>
2024-01-08 13:10:20 +0300

    custom collection view added.

---
+++ GHFollowers/CustomViews/Cells/FollowerCollectionViewCell.swift
@@ -0,0 +1,48 @@
+//
+//  FollowerCollectionViewCell.swift
+//  GHFollowers
+//
+//  Created by Burkay Atar on 4.01.2024.
+//
+
+import UIKit
+
+class FollowerCollectionViewCell: UICollectionViewCell {
+    static let reuseID = "FollowerCell"
+    
+    let avatarImageView = GHAvatarImageView(frame: .zero)
+    let usernameLabel = GHTitleLabel(textAlignment: .center, fontSize: 16)
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func set(follower: Follower) {
+        usernameLabel.text = follower.login
+        avatarImageView.downloadImage(from: follower.avatarUrl)
+    }
+    
+    private func configure() {
+        let padding: CGFloat = 8
+        
+        addSubview(avatarImageView)
+        addSubview(usernameLabel)
+        
+        NSLayoutConstraint.activate([
+            avatarImageView.topAnchor.constraint(equalTo: topAnchor, constant: padding),
+            avatarImageView.leadingAnchor.constraint(equalTo: leadingAnchor, constant: padding),
+            avatarImageView.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -padding),
+            avatarImageView.heightAnchor.constraint(equalTo: avatarImageView.widthAnchor),
+            
+            usernameLabel.topAnchor.constraint(equalTo: avatarImageView.bottomAnchor, constant: 12),
+            usernameLabel.leadingAnchor.constraint(equalTo: leadingAnchor, constant: padding),
+            usernameLabel.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -padding),
+            usernameLabel.heightAnchor.constraint(equalToConstant: 20)
+        ])
+    }
+}
