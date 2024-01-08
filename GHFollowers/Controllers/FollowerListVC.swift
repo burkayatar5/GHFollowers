9aac8ba5970fa7fcc2f2c2b8a3b5cda02ea397c5
Burkay Atar <burkayatar5@gmail.com>
2024-01-08 13:09:20 +0300

    file refactored, collection view added

--- GHFollowers/Controllers/FollowerListVC.swift
+++ GHFollowers/Controllers/FollowerListVC.swift
@@ -9,25 +9,78 @@ import UIKit
 
 class FollowerListVC: UIViewController {
     
+    enum Section {
+        case main
+    }
+    
     var username: String?
+    var followers: [Follower] = []
+    
+    var collectionView: UICollectionView?
+    var dataSource: UICollectionViewDiffableDataSource<Section, Follower>?
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .systemBackground
-        navigationController?.isNavigationBarHidden = false
-        navigationController?.navigationBar.prefersLargeTitles = true
-        
+        configureViewController()
+        configureCollectionView()
+        getFollowers()
+        configureDataSource()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.setNavigationBarHidden(false, animated: true)
     }
-    */
+    
+    private func configureViewController() {
+        view.backgroundColor = .systemBackground
+        navigationController?.navigationBar.prefersLargeTitles = true
+    }
+    
+    private func configureCollectionView() {
+        collectionView = UICollectionView(frame: view.bounds, collectionViewLayout: UIHelper.createThreeColumnFlowLayout(in: view))
+        guard let collectionView = collectionView else { return }
+        view.addSubview(collectionView)
+        collectionView.backgroundColor = .systemBackground
+        collectionView.register(FollowerCollectionViewCell.self, forCellWithReuseIdentifier: "FollowerCell")
+    }
+    
+    
+    
+    private func getFollowers() {
+        if let username = self.username {
+            NetworkManager.shared.getFollowers(for: username, page: 1) { [weak self] result in
+                guard let self = self else { return }
+                switch result {
+                    case .success(let followers):
+                        self.followers = followers
+                        self.updateData()
+                    
+                    case .failure(let error):
+                        self.presentGHAlertOnMainThread(title: "Bad Stuff Happened",
+                                                        message: error.rawValue,
+                                                        buttonTitle: "Ok")
+                }
+            }
+        }
+    }
+    
+    private func configureDataSource() {
+        guard let collectionView = collectionView else { return }
+        dataSource = UICollectionViewDiffableDataSource<Section, Follower>(collectionView: collectionView, cellProvider: { collectionView, indexPath, follower in
+            let cell = collectionView.dequeueReusableCell(withReuseIdentifier: FollowerCollectionViewCell.reuseID, for: indexPath) as! FollowerCollectionViewCell
+            cell.set(follower: follower)
+            return cell
+        })
+    }
+    
+    func updateData() {
+        var snapshot = NSDiffableDataSourceSnapshot<Section, Follower>()
+        snapshot.appendSections([.main])
+        snapshot.appendItems(followers)
+        DispatchQueue.main.async {
+            self.dataSource?.apply(snapshot, animatingDifferences: true)
+        }
+    }
 
 }
