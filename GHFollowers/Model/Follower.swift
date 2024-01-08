0c94784bea5c9a7de63ffb70b3f557149d731aca
Burkay Atar <burkayatar5@gmail.com>
2024-01-08 13:10:40 +0300

    model added.

---
+++ GHFollowers/Model/Follower.swift
@@ -0,0 +1,13 @@
+//
+//  Follower.swift
+//  GHFollowers
+//
+//  Created by Burkay Atar on 3.01.2024.
+//
+
+import Foundation
+
+struct Follower: Codable, Hashable {
+    var login: String
+    var avatarUrl: String
+}
