70a861a41265cc8866a9b71bef36081d0b7044b6
Burkay Atar <burkayatar5@gmail.com>
2024-01-09 19:25:26 +0300

    search functionality added.

--- GHFollowers/Controllers/FollowerListVC.swift
+++ GHFollowers/Controllers/FollowerListVC.swift
@@ -15,8 +15,10 @@ class FollowerListVC: UIViewController {
     
     var username: String?
     var followers: [Follower] = []
+    var filteredFollowers: [Follower] = []
     var page: Int = 1
     var hasMoreFollowers: Bool = true
+    var isSearching = false
     
     var collectionView: UICollectionView?
     var dataSource: UICollectionViewDiffableDataSource<Section, Follower>?
@@ -24,6 +26,7 @@ class FollowerListVC: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         configureViewController()
+        configureSearchController()
         configureCollectionView()
         
         if let username = username {
@@ -52,7 +55,15 @@ class FollowerListVC: UIViewController {
         collectionView.register(FollowerCollectionViewCell.self, forCellWithReuseIdentifier: "FollowerCell")
     }
     
-    
+    private func configureSearchController() {
+        let searchController = UISearchController()
+        searchController.searchResultsUpdater = self
+        searchController.searchBar.delegate = self
+        //searchController.obscuresBackgroundDuringPresentation = false -> to not obscure the collection view when typing.
+        searchController.searchBar.placeholder = "Search for a username."
+        navigationItem.searchController = searchController
+        
+    }
     
     private func getFollowers(username: String, page: Int) {
         showLoadingView()
@@ -70,7 +81,16 @@ class FollowerListVC: UIViewController {
                     */
                     if followers.count < 100 { self.hasMoreFollowers = false }
                         self.followers.append(contentsOf: followers)
-                        self.updateData()
+                    
+                        if self.followers.isEmpty {
+                            let message = "This user does not have any followers. Go follow them 😀."
+                            DispatchQueue.main.async {
+                                self.showEmptyStateView(with: message, in: self.view)
+                            }
+                            return
+                        }
+                    
+                        self.updateData(on: self.followers)
                     
                     case .failure(let error):
                         self.presentGHAlertOnMainThread(title: "Bad Stuff Happened",
@@ -90,7 +110,7 @@ class FollowerListVC: UIViewController {
         })
     }
     
-    func updateData() {
+    func updateData(on followers: [Follower]) {
         var snapshot = NSDiffableDataSourceSnapshot<Section, Follower>()
         snapshot.appendSections([.main])
         snapshot.appendItems(followers)
@@ -119,5 +139,29 @@ extension FollowerListVC: UICollectionViewDelegate {
                 getFollowers(username: username, page: page)
             }
         }
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        let activeArray = isSearching ? filteredFollowers : followers
+        let follower = activeArray[indexPath.item]
+        
+        let destinationVC = UserInfoVC()
+        destinationVC.username = follower.login
+        let navController = UINavigationController(rootViewController: destinationVC)
+        present(navController, animated: true)
+    }
+}
+
+extension FollowerListVC: UISearchResultsUpdating, UISearchBarDelegate {
+    func updateSearchResults(for searchController: UISearchController) {
+        guard let filter = searchController.searchBar.text, !filter.isEmpty else { return }
+        isSearching = true
+        filteredFollowers = followers.filter({ $0.login.lowercased().contains(filter.lowercased()) })
+        updateData(on: filteredFollowers)
+    }
+    
+    func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
+        isSearching = false
+        updateData(on: followers)
     }
 }
