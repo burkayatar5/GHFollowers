ab4b0fd1e2ab83cd215b951ea02e90eadbf1ee2f
Burkay Atar <burkayatar5@gmail.com>
2024-01-15 19:22:09 +0300

    Constants file added.

---
+++ GHFollowers/Utilities/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  GHFollowers
+//
+//  Created by Burkay Atar on 14.01.2024.
+//
+
+import Foundation
+
+enum SFSymbols {
+    static let location = "mappin.and.ellipse"
+}
