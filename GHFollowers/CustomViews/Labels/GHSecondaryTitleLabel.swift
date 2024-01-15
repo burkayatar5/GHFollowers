3d5555b48927d020abb1ef738d6cabf356de88a3
Burkay Atar <burkayatar5@gmail.com>
2024-01-15 19:24:02 +0300

    secondary title label added.

---
+++ GHFollowers/CustomViews/Labels/GHSecondaryTitleLabel.swift
@@ -0,0 +1,35 @@
+//
+//  GHSecondaryTitleLabel.swift
+//  GHFollowers
+//
+//  Created by Burkay Atar on 14.01.2024.
+//
+
+import UIKit
+
+class GHSecondaryTitleLabel: UILabel {
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    init(fontSize: CGFloat) {
+        super.init(frame: .zero)
+        self.font = UIFont.systemFont(ofSize: fontSize, weight: .medium)
+        configure()
+    }
+    
+    /// for font we use preferredFont property of UIFont to provide dynamic text support.
+    private func configure() {
+        textColor = .secondaryLabel
+        adjustsFontSizeToFitWidth = true
+        minimumScaleFactor = 0.90
+        lineBreakMode = .byTruncatingTail
+        translatesAutoresizingMaskIntoConstraints = false
+    }
+}
